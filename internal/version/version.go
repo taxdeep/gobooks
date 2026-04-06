99f5ca12989232b751be13469c79c33112507388
GoBooks CI <ci@gobooks.local>
2026-04-06 23:03:11 +0000

    chore(version): bump to 0.0.9.26040623

--- internal/version/version.go
+++ internal/version/version.go
@@ -2,5 +2,4 @@
 package version
 
 // Version is the semantic version of the GoBooks binary.
-// X.X.X.YYMMDDHH X.X.X为大版本,有新的模块，新的功能才会升. YYMMDDHH为调整，功能优化。
-const Version = "0.0.9.26040600"
+const Version = "0.0.9.26040623"
